9016d3df3a2226910c62634be204e6e0d72679d0
Angela Rojas <nataliart4@gmail.com>
2019-12-12 08:57:07 -0500

    Fixing readme and refactoring on map function

--- src/services/github-services.ts
+++ src/services/github-services.ts
@@ -11,27 +11,29 @@ export interface RepositoryData {
   updated_at: string;
 }
 
-const generateResponse = (message: string, repositories: []) => ({message: message, repositories: repositories});
+const generateResponse = (message: string, repositories: []) => ({ message: message, repositories: repositories });
 
 export const getProjectsByUser = (username: string): Promise<ProjectByUserResponse> => {
   const path = `/users/${username}/repos`;
   const url = [GITHUB_URL, path].join('');
 
   return fetch(url, { method: 'GET', headers: { 'Accept': 'application/vnd.github.inertia-preview+json' } })
-  .then(response => response.json())
-  .then(repositories => {
-    if(repositories.message){
-      return Promise.reject(generateResponse('Not found repositories :(', [])); //TODO handle error
-    }
+    .then(response => response.json())
+    .then(repositories => {
+      if (repositories.message) {
+        return Promise.reject(generateResponse('Not found repositories :(', [])); //TODO handle error
+      }
 
-    const repos = repositories.map((repo: any) => {
-      return { name: repo.name, created_at: repo.created_at, updated_at: repo.updated_at }; // TODO refact
+      const repos = repositories.map((repo: any) => ({
+        name: repo.name, created_at:
+          repo.created_at, updated_at:
+          repo.updated_at
+      }));
+
+      return Promise.resolve(generateResponse(`Found ${repos.length} repositories :)`, repos));
+    })
+    .catch(error => {
+      console.error('Error on retrieving repos: ', error);
+      return Promise.reject({ error: 'Something wrong happened when calling GitHub API' });
     });
-
-    return Promise.resolve(generateResponse(`Found ${repos.length} repositories :)`, repos ));
-  })
-  .catch(error => {
-    console.error('Error on retrieving repos: ', error);
-    return Promise.reject({error: 'Something wrong happened when calling GitHub API'});
-  });
 }
