7f2fee3bb699b4dc858cb25f67ab34b67b277226
Angela Rojas <nataliart4@gmail.com>
2019-12-12 10:51:43 -0500

    Add jest-fetch-mock

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+import { GlobalWithFetchMock } from "jest-fetch-mock";
+
+const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
+customGlobal.fetch = require('jest-fetch-mock');
+customGlobal.fetchMock = customGlobal.fetch;
