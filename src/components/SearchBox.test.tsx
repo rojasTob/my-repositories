5267123516bf08938dc06ae6fffbef1a68ff8063
Angela Rojas <nataliart4@gmail.com>
2019-12-13 10:24:21 -0500

    Add tests for hooks

--- src/components/SearchBox.test.tsx
+++ src/components/SearchBox.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { act, Simulate } from 'react-dom/test-utils';
 
 import SearchBox from './SearchBox';
 
@@ -33,5 +33,20 @@ describe('SearchBox', () => {
   test('has a button', () => {
     const button = container.querySelector('[id=searchButton]');
     expect(button).not.toBe(null);
+  });
+
+  test('changes the user state when typing text on input "username"', () => {
+    const input = container.querySelector('[id=username]');
+    Simulate.change(input, { target: { value: 'a-user-name' } } as any); //TODO 'any' is not a good solution :(
+    expect(input.value).toBe("a-user-name");
+  });
+
+  test('executes the function when clicking the button', () => {
+    const input = container.querySelector('[id=username]');
+    Simulate.change(input, { target: { value: 'a-user-name' } } as any);
+
+    const button = container.querySelector('[id=searchButton]');
+    Simulate.click(button);
+    expect(searchFn).toBeCalledWith('a-user-name');
   });
 });
